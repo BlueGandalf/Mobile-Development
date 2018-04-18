bb25b9cf7f8e3b7df43a923c2ce4866c21e2570c
BlueGandalf <pksmith137@gmail.com>
2018-04-18 16:36:06 +0100

    add recyclerview

---
+++ Examples/Recyclerview/app/src/main/java/com/example/pksmi/recyclerviewexample/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.pksmi.recyclerviewexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private String[] startingArray = { "A", "B", "2", "D" };
+    private ArrayList<String> myDataset = new ArrayList<String>(Arrays.asList(startingArray));
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    public void newList(View view){
+        EditText editText = findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        myDataset.add(message);
+        mAdapter.notifyItemInserted(myDataset.size() - 1);
+    }
+
+
+}
